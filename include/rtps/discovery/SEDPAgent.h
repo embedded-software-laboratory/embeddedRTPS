dd8371050ba9e14ddcb4261a346d4448ccb2d37b
ext-kampmann <kampmann@embedded.rwth-aachen.de>
2020-01-14 07:04:35 +0100

    correcting spelling

--- include/rtps/discovery/SEDPAgent.h
+++ include/rtps/discovery/SEDPAgent.h
@@ -17,7 +17,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE
 
-This file is part of Embedded RTPS.
+This file is part of embeddedRTPS.
 
 Author: i11 - Embedded Software, RWTH Aachen University
 */
